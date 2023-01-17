8426d66de60213ca6fda2ae4bb7ba70566892399
Kyu Lee <kwooler@gmail.com>
2023-01-17 07:43:39 -0800

    CS07

---
+++ NameMain/file01.py
@@ -0,0 +1,5 @@
+# File 1.py : the module name is File1
+
+import file02
+
+print('File 1: __name__ is set to ', __name__)
---
+++ NameMain/file02.py
@@ -0,0 +1,3 @@
+# File 2.py
+
+print('File 2: __name__ is set to ', __name__)
